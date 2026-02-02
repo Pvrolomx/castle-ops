b039fbb2e22b76eac003458997ff7fc58dc30d29
Pvrolomx <pvrolo@gmail.com>
2026-02-02 16:55:24 -0600

    fix: force dynamic rendering

--- app/incidents/new/page.tsx
+++ app/incidents/new/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { useEffect, useState } from 'react'
 import { supabase, Provider } from '@/lib/supabase'
 import { useRouter } from 'next/navigation'
